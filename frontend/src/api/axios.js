551a67d3735b720be7771f088f34a9a6d6f6eb17
Gyrateplace <gyrateplace@gmail.com>
2025-11-11 08:17:38 -0500

    cambios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://wheels-9og0.onrender.com/api",
+  baseURL: "https://wheels-peka.onrender.com",
   withCredentials: true,
 });
 
