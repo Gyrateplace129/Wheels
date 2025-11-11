c89b6d77a231ea5598fc18963e1a7e164b3d11cd
Gyrateplace <gyrateplace@gmail.com>
2025-11-11 07:06:34 -0500

    cambios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </AuthProvider>
