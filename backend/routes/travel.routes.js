02dd4a02b015bce496f3bca9beb4b1c249ebb4c0
Gyrateplace <gyrateplace@gmail.com>
2025-11-13 10:04:45 -0500

    cambios

--- backend/routes/travel.routes.js
+++ backend/routes/travel.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createTravel, getAvailableTravels, reserveTravel } from "../controllers/travel.controllers.js";
-import verifyToken from "../middlewares/auth.middleware.js";
+import verifyToken from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
